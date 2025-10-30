e8942f341b8028af5562fcde257c86f7a8826419
Tachiyama Kotarou <lishanhutailang3@gmail.com>
2025-10-30 11:13:02 +0900

    Bossクラスの作成

--- FantasyAction/Game/Item.h
+++ FantasyAction/Game/Item.h
@@ -5,9 +5,17 @@ public:
 	bool Start();
 	void Update();
 	void Rotation();
+
+	void SetPosition(const Vector3& position)
+	{
+		m_position = position;
+	}
+
 	void Render(RenderContext& rc);
 
 	ModelRender m_shellRender;
+private:
+	
 	Vector3     m_position;
 	Vector3     m_scale;
 	Quaternion  m_rotation;
@@ -18,10 +26,6 @@ class Shell : public Item
 public:
 	bool Start();
 	void Collision();
-	void SetPosition(const Vector3& position)
-	{
-		m_position = position;
-	}
 
 	Vector3     m_moveSpeed;
 };
